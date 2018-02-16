abab66d54e06bcd6d13fb4546870aaab22043619
koleberd <dkoleber@asu.edu>
2018-02-16 10:44:16 -0700

    smol changes

--- src/main.py
+++ src/main.py
@@ -27,7 +27,7 @@ world_benchmark_set_dir = 'json/benchmark_sets/'
 
 #--- SIMULATION PARAMETERS ---
 RAND_SPAWN = True
-sim_name = 'rv_2'
+sim_name = 'rv_4'
 bl_ind = {None:0,'wood':1,'stone':2,'crafting bench':3,'iron ore':4,'coal':5,'furnace':6,'wall':7} #for encoding data for input into nn
 action_set = ['locateObject:wood','locateObject:stone','locateObject:crafting bench','locateObject:iron ore','locateObject:coal'] #for decoding data from output of nn
 simulation_config_name = sim_config_dir + sim_name + '.json'
@@ -41,15 +41,17 @@ hidden2_dim = 256
 
 #--- TRAINING PARAMETERS ---
 FRAME_SKIP_THRESHOLD = .45 #pick frame if random number [0,1] under threshold
-FRAME_DENSITY_THRESHOLD = .09 #pick frame if frame density over threshold (frame density is ratio of non-empty units to all units in input kernel)
-MOVING_AVERAGE_SIZE = 10 #moving average is compared to current run to determine if current run should be positively or negatively reinforced
+FRAME_DENSITY_THRESHOLD = .1 #pick frame if frame density over threshold (frame density is ratio of non-empty units to all units in input kernel)
+MOVING_AVERAGE_SIZE = 15 #moving average is compared to current run to determine if current run should be positively or negatively reinforced
 OUTPUT_LOWER_BIAS = .25 #lowest possible output weight (max is always 1)
-DROPOUT_TRAINING = 1.0 #chance a node will be kept during dropout during training
-TRAINGING_ROUNDS = 200 #number of rounds to train
-LEARNING_RATE = 0.01
-BATCH_CUTOFF = 2000
-VALIDATION_ROUNDS = 20
-BENCHMARK_SAMPLES = 20
+DROPOUT_TRAINING = 1.0 #chance a neuron will be kept during dropout during training
+TRAINGING_ROUNDS = 100 #number of rounds to train
+LEARNING_RATE = 0.001
+BATCH_CUTOFF = 2500
+VALIDATION_ROUNDS = 20 #must be less than MAX_BENCHMARK_SAMPLES
+BENCHMARK_SAMPLES = 20 #must be less than MAX_BENCHMARK_SAMPLES
+MAX_BENCHMARK_SAMPLES = 100
+MODELS_TO_GENERATE = 10
 
 
 #gets average distance for every object from every open position for a world, and puts it in a json file
@@ -69,20 +71,20 @@ def getWorldCosts(world,world_name):
 
     with open(flnm) as wldcf:
         return json.load(wldcf)
-
 def getWorldBenchmarkPositions(world,world_name):
     flnm = world_benchmark_set_dir + world_name + '_positions.json'
     if not os.path.isfile(flnm):
         positions = []
-        while len(positions) < BENCHMARK_SAMPLES:
+        while len(positions) < MAX_BENCHMARK_SAMPLES:
             r_pos = world.randomizePos()
             if r_pos not in positions:
                 positions.append(r_pos)
         with open(flnm,'w+') as posjs:
             json.dump({'positions':positions},posjs,indent=4)
     with open(flnm) as posjs:
-        return json.load(posjs)['positions']
-
+        positions = json.load(posjs)['positions']
+        positions = [(x[0],x[1]) for x in positions]
+        return positions
 def getWorldDensities():
     simulation_config_name = sim_config_dir + sim_name + '.json'
     with open(simulation_config_name) as simjs:
@@ -239,7 +241,6 @@ def generateBatch(stats,averages):
 
     labels = [sim_label for x in range(0,len(inputs))]
     return (inputs,labels),sim_averages
-
 def generateAnalysis(in_flnm,out_flnm):
     with open(in_flnm) as sim_js:
         sim_stats = json.load(sim_js)
@@ -355,103 +356,116 @@ def train():
     train_step = optimizer.minimize(loss)
 
 
-
+    valid_batches = 0
     moving_averages = [[average_costs[x] for x in action_set]]
-    with tf.Session() as sess:
-        training_performance = []
-        total_time = time.time()
-        sess.run(tf.global_variables_initializer())
-        saver = tf.train.Saver()
-        tf.add_to_collection('n_input',input_tensor)
-        tf.add_to_collection('n_output',output_tensor)
-        tf.add_to_collection('n_dropout',dropout_rate)
+    while valid_batches < MODELS_TO_GENERATE:
+        with tf.Session() as sess:
 
-        #--- INITIAL BENCHMARK ---
-        init_avg = 0
-        init_valid_rounds = 0
-        init_time_sum = 0
-        for step in range(VALIDATION_ROUNDS):
+            print('--- starting training for next model ---')
+            bad_batch = False
+            training_performance = []
+            total_time = time.time()
+            sess.run(tf.global_variables_initializer())
+            saver = tf.train.Saver()
+            tf.add_to_collection('n_input',input_tensor)
+            tf.add_to_collection('n_output',output_tensor)
+            tf.add_to_collection('n_dropout',dropout_rate)
 
-            sim_out,sim_len,sim_time = run2d3d(sim_name,select_method = lambda x,frame,prev,prev_time: selectCheapestDNN(x,frame,prev,prev_time,output_tensor),random_spawn=False,spawn_pos=validation_pos[step])
-            print('Benchmarking... round ' + str(step+1) + '/' + str(VALIDATION_ROUNDS) + ' - ' + str(sim_len))
-            init_time_sum += sim_time
-            if sim_len != -1:
-                init_avg += sim_len
-                init_valid_rounds += 1
+            #--- INITIAL BENCHMARK ---
+            init_avg = 0
+            init_valid_rounds = 0
+            init_time_sum = 0
+            for step in range(VALIDATION_ROUNDS):
 
-        init_avg = float(init_avg)/float(init_valid_rounds)
-        print('initial average performance: ' + str(init_avg) + ' over ' + str(init_valid_rounds) + ' rounds')
-        est_train_time = ((float(init_time_sum)/float(VALIDATION_ROUNDS)) * float(TRAINGING_ROUNDS)) / 60.0
-        print(str(init_time_sum).split('.')[0] + 's to execute initial test.\n Expect ' + str(est_train_time)[:7] + 'min to train, plus another ' + str(float(init_time_sum)/60.0)[:7] + 'min to validate')
+                sim_out,sim_len,sim_time = run2d3d(sim_name,select_method = lambda x,frame,prev,prev_time: selectCheapestDNN(x,frame,prev,prev_time,output_tensor),random_spawn=False,spawn_pos=benchmark_positions[step])
+                print('Benchmarking... round ' + str(step+1) + '/' + str(VALIDATION_ROUNDS) + ' - ' + str(sim_len))
+                init_time_sum += sim_time
+                if sim_len != -1:
+                    init_avg += sim_len
+                    init_valid_rounds += 1
 
-        #--- TRAINING ---
-        for step in range(0,TRAINGING_ROUNDS):
-            sim_out,sim_len,sim_time = run2d3d(sim_name,select_method = lambda x,frame,prev,prev_time: selectCheapestDNN(x,frame,prev,prev_time,output_tensor),random_spawn=RAND_SPAWN)
+            init_avg = float(init_avg)/float(init_valid_rounds)
+            print('initial average performance: ' + str(init_avg) + ' over ' + str(init_valid_rounds) + ' rounds')
+            est_train_time = ((float(init_time_sum)/float(VALIDATION_ROUNDS)) * float(TRAINGING_ROUNDS)) / 60.0
+            print(str(init_time_sum).split('.')[0] + 's to execute initial test.\n Expect ' + str(est_train_time)[:7] + 'min to train, plus another ' + str(float(init_time_sum)/60.0)[:7] + 'min to validate')
 
-            if sim_len == -1:
-                print('Bad batch')
-                continue
+            bad_batches = 0
+            #--- TRAINING ---
+            for step in range(0,TRAINGING_ROUNDS):
+                sim_out,sim_len,sim_time = run2d3d(sim_name,select_method = lambda x,frame,prev,prev_time: selectCheapestDNN(x,frame,prev,prev_time,output_tensor),random_spawn=RAND_SPAWN)
 
-            #calculate moving average to compare current performance against
-            current_averages = [0 for x in range(len(action_set))]
-            for x in moving_averages:
-                for i in range(len(action_set)):
-                    current_averages[i] += x[i]
-            current_averages = [float(x)/float(len(moving_averages)) for x in current_averages]
-            batch, sim_averages = generateBatch(sim_out,current_averages)
-            moving_averages = moving_averages[1:]
-            moving_averages.append(sim_averages)
+                if sim_len == -1:
+                    print('Bad batch')
+                    bad_batches += 1
+                    if bad_batches > 2:
+                        bad_batch = True
+                        break
+                    else:
+                        continue
+                    #continue
 
-            current_loss = loss.eval(feed_dict={input_tensor: batch[0], label_tensor: batch[1], dropout_rate: 1.0})
-            print('training... round ' + str(step+1) + '/' + str(TRAINGING_ROUNDS) + ' - performance: ' + str(sim_len) + ' - samples: ' + str(len(batch[0])) + ' - loss: ' + str(current_loss))
-            training_performance.append({'performance':sim_len,'loss':float(current_loss)})
-            train_step.run(feed_dict={input_tensor: batch[0], label_tensor: batch[1], dropout_rate: DROPOUT_TRAINING})
+                #calculate moving average to compare current performance against
+                current_averages = [0 for x in range(len(action_set))]
+                for x in moving_averages:
+                    for i in range(len(action_set)):
+                        current_averages[i] += x[i]
+                current_averages = [float(x)/float(len(moving_averages)) for x in current_averages]
+                batch, sim_averages = generateBatch(sim_out,current_averages)
+                moving_averages = moving_averages[1:]
+                moving_averages.append(sim_averages)
 
-        #--- FINAL BENCHMARK ---
-        end_avg = 0
-        end_valid_rounds = 0
-        end_time_sum = 0
-        for step in range(VALIDATION_ROUNDS):
+                current_loss = loss.eval(feed_dict={input_tensor: batch[0], label_tensor: batch[1], dropout_rate: 1.0})
+                print('training... round ' + str(step+1) + '/' + str(TRAINGING_ROUNDS) + ' - performance: ' + str(sim_len) + ' - samples: ' + str(len(batch[0])) + ' - loss: ' + str(current_loss))
+                training_performance.append({'performance':sim_len,'loss':float(current_loss)})
+                train_step.run(feed_dict={input_tensor: batch[0], label_tensor: batch[1], dropout_rate: DROPOUT_TRAINING})
 
-            sim_out,sim_len,sim_time = run2d3d(sim_name,select_method = lambda x,frame,prev,prev_time: selectCheapestDNN(x,frame,prev,prev_time,output_tensor),random_spawn=False,spawn_pos=validation_pos[step])
-            print('Validating... round ' + str(step+1) + '/' + str(VALIDATION_ROUNDS) + ' - ' + str(sim_len))
-            end_time_sum += sim_time
-            if sim_len != -1:
-                end_avg += sim_len
-                end_valid_rounds += 1
+            if not bad_batch:
+                #--- FINAL BENCHMARK ---
+                end_avg = 0
+                end_valid_rounds = 0
+                end_time_sum = 0
+                for step in range(VALIDATION_ROUNDS):
 
-        #--- OUTPUT STATS ---
-        total_time = time.time() - total_time
-        end_avg = float(end_avg)/float(end_valid_rounds)
-        improvement = end_avg/init_avg
-        print('final average performance: ' + str(end_avg) + ' over ' + str(end_valid_rounds) + ' rounds')
-        print('total time elapsed: ' + str(total_time) + ' s')
-        print('performance increase of ' + str(improvement))
-        data = {'stats':training_performance,
-                'init benchmark':init_avg,
-                'final benchmark':end_avg,
-                'benchmark size':VALIDATION_ROUNDS,
-                'training size':TRAINGING_ROUNDS,
-                'world configuration':simulation_config_name,
-                'dropout':DROPOUT_TRAINING,
-                'frame skip threshold':FRAME_SKIP_THRESHOLD,
-                'frame density threshold':FRAME_DENSITY_THRESHOLD,
-                'hidden 1 dim': hidden1_dim,
-                'hidden 2 dim': hidden2_dim,
-                'performance improvement':improvement,
-                'random spawn during training':str(RAND_SPAWN)
-                }
-        with open(sim_stat_dir + sim_name + '-' + str(time.time())[5:10] + '.json','w+') as output_json:
-            json.dump(data,output_json,indent=4,sort_keys=True)
+                    sim_out,sim_len,sim_time = run2d3d(sim_name,select_method = lambda x,frame,prev,prev_time: selectCheapestDNN(x,frame,prev,prev_time,output_tensor),random_spawn=False,spawn_pos=benchmark_positions[step])
+                    print('Validating... round ' + str(step+1) + '/' + str(VALIDATION_ROUNDS) + ' - ' + str(sim_len))
+                    end_time_sum += sim_time
+                    if sim_len != -1:
+                        end_avg += sim_len
+                        end_valid_rounds += 1
 
-        #--- SAVE MODEL IF EFFECTIVE ---
-        if improvement < 1.0:
-            m_name = sim_name + '_' + str(improvement)
-            print('Saving model "' + m_name + '"')
-            saver.save(sess,'trainedModels/' + m_name)
-            m_name = 'trainedModels/' + m_name
-            benchmarkAgainstAlternates(m_name)
-        return improvement
+                #--- OUTPUT STATS ---
+                total_time = time.time() - total_time
+                end_avg = float(end_avg)/float(end_valid_rounds)
+                improvement = end_avg/init_avg
+                print('final average performance: ' + str(end_avg) + ' over ' + str(end_valid_rounds) + ' rounds')
+                print('total time elapsed: ' + str(total_time) + ' s')
+                print('performance increase of ' + str(improvement))
+                data = {'stats':training_performance,
+                        'init benchmark':init_avg,
+                        'final benchmark':end_avg,
+                        'benchmark size':VALIDATION_ROUNDS,
+                        'training size':TRAINGING_ROUNDS,
+                        'world configuration':simulation_config_name,
+                        'dropout':DROPOUT_TRAINING,
+                        'frame skip threshold':FRAME_SKIP_THRESHOLD,
+                        'frame density threshold':FRAME_DENSITY_THRESHOLD,
+                        'hidden 1 dim': hidden1_dim,
+                        'hidden 2 dim': hidden2_dim,
+                        'performance improvement':improvement,
+                        'random spawn during training':str(RAND_SPAWN)
+                        }
+                with open(sim_stat_dir + sim_name + '-' + str(time.time())[5:10] + '.json','w+') as output_json:
+                    json.dump(data,output_json,indent=4,sort_keys=True)
+
+                #--- SAVE MODEL IF EFFECTIVE ---
+                if improvement < 1.0:
+                    m_name = sim_name + '_' + str(improvement)
+                    print('Saving model "' + m_name + '"')
+                    saver.save(sess,'trainedModels/' + m_name)
+                    m_name = 'trainedModels/' + m_name
+                    #benchmarkAgainstAlternates(m_name)
+                valid_batches += 1
+        #return improvement
 
 def benchmarkAgainstAlternates(model_name):
     simulation_config_name = sim_config_dir + sim_name + '.json'
@@ -463,7 +477,7 @@ def benchmarkAgainstAlternates(model_name):
      #load world costs
     temp_world = GameWorld2d(simcf['world_location'],spawn_random=False)
     average_costs = getWorldCosts(temp_world,world_name)
-    sample_locs = [temp_world.randomizePos() for x in range(BENCHMARK_SAMPLES)]
+    benchmark_positions = getWorldBenchmarkPositions(temp_world,world_name)
 
     #load world benchmark positions
     benchmark_positions = getWorldBenchmarkPositions(temp_world,world_name)
@@ -483,10 +497,10 @@ def benchmarkAgainstAlternates(model_name):
         for s_name in selection_set:
             avg = 0
             count = 0
-            for loc in sample_locs:
+            for loc in benchmark_positions[:BENCHMARK_SAMPLES]:
 
                 sim_out,sim_len,sim_time = run2d3d(sim_name,select_method = selection_set[s_name],random_spawn=False,spawn_pos=loc)
-                print('running ' + str(count+1) + '/' + str(len(sample_locs)) + ' for ' + s_name + ' - ' + str(sim_len))
+                print('running ' + str(count+1) + '/' + str(len(benchmark_positions)) + ' for ' + s_name + ' - ' + str(sim_len))
                 if sim_len != -1:
                     avg += sim_len
                     count += 1
@@ -498,7 +512,7 @@ def benchmarkAgainstAlternates(model_name):
 #run2d3d('json/simulation_configs/rv_1.json',select_method = lambda x: selectUser(x))
 #sim = run2d3d('json/simulation_configs/rv_1.json',select_method = lambda x,f: selectCheapest(x,f))
 #print(sim[1])
-#train()
+train()
 #benchmarkAgainstAlternates('trainedModels/rv_2_0.5392337205024439')
 #getWorldDensities()
-benchmarkAgainstAlternates('trainedModels/rv_3_0.7041499330655957')
+#benchmarkAgainstAlternates('trainedModels/rv_3_0.7041499330655957')
